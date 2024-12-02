f241f4bae5e1a550b07c0fc156d10e780ce0a692
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2024-12-02 09:27:34 -0300

    2024 day1 solved

--- 2024/day1/main.zig
+++ 2024/day1/main.zig
@@ -1,30 +1,28 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Hello, world!\n", .{});
+const inputData = @embedFile("input.txt");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+
+    const parsedInput = try parseInput(alloc, inputData);
+
+    const part1 = try solve1(parsedInput);
+    const part2 = try solve2(alloc, parsedInput);
+
+    std.debug.print("Part 1: {}\n", .{part1});
+    std.debug.print("Part 2: {}\n", .{part2});
+
 }
 
-test "test with example input" {
-    const input =
-        \\3   4
-        \\4   3
-        \\2   5
-        \\1   3
-        \\3   9
-        \\3   3
-    ;
-
-
-    const alloc = std.testing.allocator;
-
+fn parseInput(alloc: std.mem.Allocator, input: []const u8) ![2]*std.ArrayList(i32) {
     var list1 = std.ArrayList(i32).init(alloc);
-    defer list1.deinit();
-
     var list2 = std.ArrayList(i32).init(alloc);
-    defer list2.deinit();
 
     const lists = [2]*std.ArrayList(i32){&list1, &list2};
-
 
     var it = std.mem.tokenizeAny(u8, input, " \n" );
 
@@ -38,12 +36,66 @@ test "test with example input" {
     std.mem.sort(i32, list1.items, {}, std.sort.asc(i32));
     std.mem.sort(i32, list2.items, {}, std.sort.asc(i32));
 
+    return lists;
+}
+
+
+pub fn solve1(input: [2]*std.ArrayList(i32)) !u32 {
+    const list1 = input[0];
+    const list2 = input[1];
+
     var count: u32 = 0;
     for (list1.items, list2.items) |fst, snd| {
-        std.log.warn("fst {} snd {}", .{fst, snd});
         count += @abs(fst - snd);
     }
 
-    try std.testing.expectEqual(11, count);
+    return count;
+}
+
+pub fn solve2(alloc: std.mem.Allocator, input: [2]*std.ArrayList(i32)) !u32 {
+    const list1 = input[0];
+    const list2 = input[1];
+
+    const last: usize = @intCast(list1.getLast());
+
+    const diff = try alloc.alloc(u32, last);
+    @memset(diff, 0);
+
+    for (list1.items) |item1| {
+        for (list2.items) |item2| {
+            if (item1 == item2) {
+                const diffIdx: usize = @intCast(item1);
+                diff[diffIdx] += @intCast(item1);
+            } 
+
+            if (item2 > item1) {
+                break;
+            }
+        }
+    }
+
+    var sum: u32 = 0;
+    for (diff) |diffVal| {
+        sum += diffVal;
+    }
+
+    return sum;
+}
+
+test "test with example input" {
+    const input =
+        \\3   4
+        \\4   3
+        \\2   5
+        \\1   3
+        \\3   9
+        \\3   3
+    ;
+
+    const alloc = std.testing.allocator;
+
+    const solution = try solve1(alloc, input);
+    
+    try std.testing.expectEqual(11, solution);
 
 }
