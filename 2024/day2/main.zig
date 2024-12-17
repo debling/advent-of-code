c80952f7343e2c1941065304c1364eb7d3b2a5fa
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2024-12-17 18:27:03 -0300

    quase

--- 2024/day2/main.zig
+++ 2024/day2/main.zig
@@ -1,88 +1,86 @@
 const std = @import("std");
 
-const Alloc = std.mem.Allocator;
-
 const inputData = @embedFile("input.txt");
 
-// pub fn main() !void {
-//     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-//     defer arena.deinit();
-//     const alloc = arena.allocator();
-//
-//
-//     const parsedInput = try parseInput(alloc, inputData);
-//     //
-//     // const part1 = try solve1(parsedInput);
-//     // const part2 = try solve2(alloc, parsedInput);
-//
-//     // std.debug.print("Part 1: {}\n", .{part1});
-//     // std.debug.print("Part 2: {}\n", .{part2});
-// }
-//
-fn parseInput(alloc: std.mem.Allocator, input: []const u8) !*const []*const[]u32 {
-    var lines = std.mem.splitScalar(u8, input, '\n');
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
 
-    var reports = std.ArrayList(*const []u32).init(alloc);
-    var currentReport = std.ArrayList(u32).init(alloc);
+    const parsedData = try parseInput(alloc, inputData);
 
-    while (lines.next()) |numSequence| {
-        std.debug.print("Line {any} \n", .{numSequence});
-        for (numSequence) |seqItem| {
-            switch (seqItem) {
-                ' ' => continue,
-                else => {
-                    const n = seqItem - '0';
-                    try currentReport.append(n); 
-                },
-            }
-        }
+    const part1 = solve1(parsedData);
+    const part2 = solve2(parsedData);
 
-        try reports.append(&(try currentReport.toOwnedSlice()));
-    }
-
-    const out = try reports.toOwnedSlice(); 
-    return &out;
+    std.debug.print("Part 1: {}\n", .{part1});
+    std.debug.print("Part 2: {}\n", .{part2});
 }
 
-fn isReportSafe(report: *const []u32) bool {
-    std.debug.assert(report.*.len > 1);
+fn parseInput(alloc: std.mem.Allocator, input: []const u8) ![][]i32 {
+    var splittedIn = std.mem.splitScalar(u8, input, '\n');
 
-    var idx: u8 = 1;
-    while (idx < report.*.len) : (idx += 1) {
-        const fst: i32 = @intCast(report.*[idx - 1]);
-        const snd: i32 = @intCast(report.*[idx]);
+    var levels = std.ArrayList([]i32).init(alloc);
 
-        const diff: i32 = fst - snd;
-        if (@abs(diff) > 2) {
+    while (splittedIn.next()) |numSequence| {
+        if (numSequence.len == 0) {
+            break;
+        }
+        var splittedReport = std.mem.tokenizeScalar(u8, numSequence, ' ');
+        var currentReport = std.ArrayList(i32).init(alloc);
+        while (splittedReport.next()) |seqItem| {
+            const n = try std.fmt.parseInt(i32, seqItem, 10);
+            try currentReport.append(n);
+        }
+
+        try levels.append(try currentReport.toOwnedSlice());
+    }
+    return levels.toOwnedSlice();
+}
+
+fn isSafe(report: []i32, tolerance: bool) bool {
+    std.debug.assert(report.len > 1);
+
+    const incrementDirection = std.math.sign(report[0] - report[1]);
+
+    var levelRemoved = false;
+    for (0..report.len - 1, 1..report.len) |fstIdx, sndIdx| {
+        const diff = report[fstIdx] - report[sndIdx];
+        const levelBad = (@abs(diff) > 3) or (incrementDirection != std.math.sign(diff));
+
+        if (levelBad and !tolerance) {
             return false;
+        } else if (levelBad and tolerance and !levelRemoved and sndIdx != report.len - 1) {
+            const diffLvlRemoved = report[fstIdx] - report[sndIdx + 1];
+            const levelStillBad = (@abs(diffLvlRemoved) > 3) or (incrementDirection != std.math.sign(diffLvlRemoved));
+
+            if (levelStillBad) {
+                return false;
+            } else {
+                levelRemoved = true;
+            }
         }
     }
 
     return true;
 }
 
-
-fn solvePart1(reports: *const[]*const[]u32) void {
-    var safeCount: u32 = 0;
-    for (reports.*) |l| {
-        std.debug.print("report {any}\n", .{l.*});
-        const isSafe: u1 = @bitCast(isReportSafe(l));
-        safeCount +=  @intCast(isSafe);
+fn solve1(input: [][]i32) u32 {
+    var sum: u32 = 0;
+    for (input) |r| {
+        const reportSafe: u1 = @intFromBool(isSafe(r, false));
+        sum += reportSafe;
     }
-    
-    std.debug.print("Part 1: {}", .{safeCount});
+    return sum;
 }
 
-pub fn main() !void {
-    const arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const alloc = arena.allocator();
-
-    const reports = try parseInput(alloc, inputData);
-
-    solvePart1(reports);
+fn solve2(input: [][]i32) u32 {
+    var sum: u32 = 0;
+    for (input) |r| {
+        const reportSafe: u1 = @intFromBool(isSafe(r, true));
+        sum += reportSafe;
+    }
+    return sum;
 }
-
 
 test "test with example input" {
     const input =
@@ -95,11 +93,28 @@ test "test with example input" {
     ;
 
     const alloc = std.testing.allocator;
-    const reports = try parseInput(alloc, input);
-    defer alloc.free(reports.*);
-    defer for (reports.*) |r| {
-        alloc.free(r.*);
+
+    const parsedData = try parseInput(alloc, input);
+    defer alloc.free(parsedData);
+    defer for (parsedData) |value| {
+        alloc.free(value);
     };
 
-    solvePart1(reports);
+    try std.testing.expectEqual(true, isSafe(parsedData[0], false));
+    try std.testing.expectEqual(false, isSafe(parsedData[1], false));
+    try std.testing.expectEqual(false, isSafe(parsedData[2], false));
+    try std.testing.expectEqual(false, isSafe(parsedData[3], false));
+    try std.testing.expectEqual(false, isSafe(parsedData[4], false));
+    try std.testing.expectEqual(true, isSafe(parsedData[5], false));
+
+    try std.testing.expectEqual(2, solve1(parsedData));
+
+    try std.testing.expectEqual(true, isSafe(parsedData[0], true));
+    try std.testing.expectEqual(false, isSafe(parsedData[1], true));
+    try std.testing.expectEqual(false, isSafe(parsedData[2], true));
+    try std.testing.expectEqual(true, isSafe(parsedData[3], true));
+    try std.testing.expectEqual(true, isSafe(parsedData[4], true));
+    try std.testing.expectEqual(true, isSafe(parsedData[5], true));
+
+    try std.testing.expectEqual(4, solve2(parsedData));
 }
